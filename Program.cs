8607e14cb556779fae9e268b77b64a565d380232
JoaoVictorBento <135682833+JoaoVictorBento@users.noreply.github.com>
2023-06-07 14:50:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2___exercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Como declarar uma variável
+
+            //juntar os textos se chama CONCATENAçÃo
+
+            String criador = "João";
+
+            int idade = 18;
+
+            Console.WriteLine("Programa criado pelo " + criador);
+
+            Console.WriteLine("Programa criado pelo " + criador);
+
+            Console.WriteLine("Programa criado pelo " + criador);
+
+            Console.WriteLine("Programa criado pelo " + criador + ", minha idade é: " + idade);
+
+            //E esse é o jeito moderno, se chama INTERPOLAÇÃO
+
+            Console.WriteLine($"meu nome é {criador} e eu tenho {idade} anos");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
